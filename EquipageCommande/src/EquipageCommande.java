1d3c0cbcafe6f680c44aeaed05d24eba044ba1df
nassimmakdi <nassimmakdi2001@gmail.com>
2024-11-27 21:26:55 +0100

    Organisation de Tout -- jusqu'a Exercice Bureau non inclu

--- EquipageCommande/src/EquipageCommande.java
+++ EquipageCommande/src/EquipageCommande.java
@@ -8,7 +8,7 @@ public class EquipageCommande extends Equipage{
 	}
 	
 	public String toString() {
-		String s = "EquipageCommandé{" ;
+		String s = "EquipageCommand{" ;
 		s += super.toString() + "}";
 		return s;
 	}
